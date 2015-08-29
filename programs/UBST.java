df21ce40c96bdbf79023da15d694259ebca65c0d
marcbezem <bezem@ii.uib.no>
2015-08-29 15:19:35 +0200

    UBST on leipzig1M < 1 min

--- programs/UBST.java
+++ programs/UBST.java
@@ -1,4 +1,3 @@
-import java.util.Iterator;
 public class UBST<Key extends Comparable<Key>, Value> {
 
 private Node root;
@@ -12,8 +11,13 @@ private class Node{
 public boolean isEmpty() {return root==null;}
 public int size() {return size(root);}
 private int size(Node r) {if (r==null){return 0;} else {return r.N;}}
-//public Iterator<Key> keysIterator() {return keys.iterator();}
 
+public void show(){show(root); StdOut.println(root.N);}
+private void show(Node r){// Depth-first left-to-right tree transversal
+  if (r!=null) {
+    show(r.left); StdOut.println(r.value + "\t" + r.key); show(r.right);
+  }
+}
 public Value get(Key key){return get(key,root);}
 private Value get(Key k, Node r){
   if (r==null) return null;
@@ -21,14 +25,14 @@ private Value get(Key k, Node r){
   if (cmp==0) return r.value;
   if (cmp<0) {return get(k,r.left);} else {return get(k,r.right);}
 }
-
-public void put(Key k, Value v) {put(k,v,root);}
-private void put(Key k, Value v, Node r) { 
-  if (r==null) {r = new Node(k,v,1); return;}
+public void put(Key k, Value v) {root = put(k,v,root);}
+private Node put(Key k, Value v, Node r) { 
+  if (r==null) {return new Node(k,v,1);}
   int cmp = k.compareTo(r.key);
-  if (cmp==0) {r.value = v; return;}
-  if (cmp<0) {put(k,v,r.left);} else {put(k,v,r.left);}
+  if (cmp==0) {r.value = v; return r;}
+  if (cmp<0) {r.left = put(k,v,r.left);} else {r.right = put(k,v,r.right);}
   r.N = size(r.left) + size(r.right) + 1; // recompute when unwinding recursion
+  return r;
 }
 
 public static void main(String[] args)  { 
@@ -37,10 +41,6 @@ public static void main(String[] args)  {
     String key = StdIn.readString(); 
     Integer i = st.get(key); if (i!=null){st.put(key,i+1);} else {st.put(key,1);}
   }
- // Iterator<String> iter = st.keysIterator();
- // while (iter.hasNext()) {
- //   String next = iter.next(); 
- //   StdOut.println(st.get(next) + "\t" + next);
- // }
+  st.show();
 }//End of main
 }//End of UBST, based on Algorithms, 4th Edition, Alg. 3.2
