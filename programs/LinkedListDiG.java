81536512c3fbdd5e0fd7b959313127c720b9f9ca
marcbezem <bezem@ii.uib.no>
2015-10-11 12:54:36 +0200

    cycles

--- programs/LinkedListDiG.java
+++ programs/LinkedListDiG.java
@@ -28,62 +28,64 @@ public String toString(){
 
 public String dfs(Integer v, boolean[] marked) {// return type String !
   marked[v] = true; 
-  String sv = v.toString(); String s = sv; // all new
-  for (Integer w : adj[v]) if (! marked[w]) s += "->"+dfs(w,marked); // some new
-  s += "(" + sv + ")"; return s; // all new
+  String sv = v.toString(); String trace = sv; // all new
+  for (Integer w : adj[v]) if (! marked[w]) trace += "->"+dfs(w,marked); // some new
+  trace += "(" + sv + ")"; return trace; // all new
 }
 
-
-public void testdfs(){
+public void testDfs(){
   for(;;) { // infinite loop
     boolean[] marked = new boolean[V];
     StdOut.print("Enter node: "); Integer n = StdIn.readInt();
     if (n < 0 || n >= V) break;
-    dfs(n,marked);
+    StdOut.println(dfs(n,marked));
     for(int v=0;v<V;v++) StdOut.print(marked[v]?v+" ":". "); 
     StdOut.println();
   }  
 }
  
-public Integer slowCyclist(Integer u, Integer v, Integer[] paths) {
+public boolean slowCyclist(Integer u, Integer v, Integer[] paths) {
   paths[v] = u;
   for (Integer w : adj[v]) 
-    if (paths[w]==null) {Integer c = slowCyclist(v,w,paths); if (c!=null) return c;} 
-    else {int x=v; while (x!=w && x!=paths[x]) x=paths[x]; // inefficient!
-          if (x==w) {paths[w]=v; return w;}} // paths[w] updated to v
-  return null;
+    if (paths[w]==null) {if (slowCyclist(v,w,paths)) return true;}
+    else {for (int x=v; x!=paths[x]; x=paths[x]) // Inefficient! Why?
+           if (x==w) return true;}
+  return false;
 }
 
-public Integer hasCycle(Integer[] paths){//returns node on cycle in paths, null if acyclic
-  Integer cycle = null;
-  for(int v=0;v<V;v++){
-    if (paths[v]==null) { cycle = slowCyclist(v,v,paths); if (cycle != null) break; }
-  }
-  return cycle;
-}
-
-public void testslow(){
+public void testSlow(){
   for(;;) { // infinite loop
   Integer[] paths = new Integer[V];
     StdOut.print("Enter node: "); Integer n = StdIn.readInt();
     if (n < 0 || n >= V) return;
-    Integer c = slowCyclist(n,n,paths);
-    if (c!=null) {
-      int x=c; do {StdOut.print(x+"<-"); x=paths[x];} while (x!=c); StdOut.println(c);}
-    else StdOut.println("no cycle");
+    if (slowCyclist(n,n,paths)) StdOut.println("cycle in subtree below " + n);
+    else StdOut.println("no cycle in subtree below " + n);
   }  
+}
+
+public boolean hasCycle(Integer[] paths){
+  for(int v=0;v<V;v++) if (paths[v]==null && slowCyclist(v,v,paths)) return true;
+  return false;
+}
+
+public void testCycle(){
+  Integer[] paths = new Integer[V];
+  if (hasCycle(paths)) StdOut.println("cycle!");
+  else StdOut.println("no cycle"); 
 }
 
 public static void main(String[] args)  {
     LinkedListDiG dg = new LinkedListDiG(new In(args[0]));
     StdOut.print(dg.toString());
-    StdOut.print("Enter test (dfs/testslow): ");
+    StdOut.print("Enter test (testDfs/testSlow/testCycle): ");
     switch (StdIn.readString()) {
-            case "dfs"      :  dg.testdfs();
+            case "testDfs"   :  dg.testDfs();
                      break;
-            case "testslow"  :  dg.testslow();
+            case "testSlow"  :  dg.testSlow();
                      break;
-           default          :   StdOut.println("test not known");
+            case "testCycle"   :  dg.testCycle();
+                     break;
+           default           :   StdOut.println("test not known");
      }
     
   }//End of main
