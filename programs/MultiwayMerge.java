f668c095ddc9cc91d4444306b809912808b95413
marcbezem <bezem@ii.uib.no>
2016-09-19 22:22:01 +0200

    Multiway using MinPQ

---
+++ programs/MultiwayMerge.java
@@ -0,0 +1,26 @@
+import edu.princeton.cs.algs4.In; import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.MinPQ;
+
+public class MultiwayMerge {// uses MinPQ, remembers stream number, no IndexMinPQ
+  
+public static void merge(In[] streams) {
+  int N = streams.length;
+  MinPQ<PolyPair<String,Integer>> pq = new MinPQ<PolyPair<String,Integer>>(N);
+  for (int i=0; i<N; i++)
+    if (!streams[i].isEmpty()) pq.insert(new PolyPair(streams[i].readString(),i));
+  while (!pq.isEmpty()){
+    PolyPair<String,Integer> x = pq.delMin(); String s = x.getFst(); Integer i = x.getSnd();
+    StdOut.print(s + " ");
+    if (!streams[i].isEmpty()) pq.insert(new PolyPair(streams[i].readString(),i));
+  }
+  StdOut.println();
+}
+
+public static void main(String[] args){
+  int N = args.length;
+  In[] streams = new In[N];
+  for (int i=0; i<N; i++) streams[i] = new In(args[i]);
+  merge(streams);
+}//End of main
+}//End of MultiwayMerge, a variation of Multiway, Algorithms - 4th Edition, p. 322
+
