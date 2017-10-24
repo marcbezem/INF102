293f1e53bcc8b966150c154d183ff24a447e2ae9
Marc Bezem <bezem@ii.uib.no>
2017-10-24 13:31:33 +0200

    removed dead code (exch)

--- programs/sorting/mergeSort/TopDownMergeSort.java
+++ programs/sorting/mergeSort/TopDownMergeSort.java
@@ -36,11 +36,6 @@ public  static void sort(Comparable[] a) {
 private static boolean less(Comparable v, Comparable w){
   return v.compareTo(w) < 0; }
 
-private static void exch(Comparable[] a, int i, int j){
-  Comparable t = a[i];
-  a[i] = a[j];
-  a[j] = t; }
-
 private static boolean show(Comparable[] a){// can be used with assert
   for (Comparable o : a) StdOut.print(o+" ");
   StdOut.println();
